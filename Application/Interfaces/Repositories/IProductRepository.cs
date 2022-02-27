a6d3f8a229fe32176f80c05aff439acc08f01aac
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 22:50:16 +0300

    created an interface of the product repository

---
+++ Application/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Models.Product;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<ProductDto>> GetAllProductsAsync();
+    }
+}
---
+++ Infastucture/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Models.Product;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infastucture.Repositories
+{
+    public class ProductRepository : BaseRepository, IProductRepository
+    {
+        public ProductRepository(IFlurlClientFactory flurlClientFactory, IHttpContextAccessor httpContextAccessor)
+            : base(flurlClientFactory, httpContextAccessor) { }
+
+        public async Task<List<ProductDto>> GetAllProductsAsync() =>
+            await flurlClient
+            .Request("/products")
+            .GetJsonAsync<List<ProductDto>>();
+    }
+}
