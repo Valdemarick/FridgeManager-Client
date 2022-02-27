6e16bd272e8685230a075a40734f9ed277544966
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 22:51:06 +0300

    created and implemented an interface of the product service

---
+++ Application/Interfaces/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Application.Models.Product;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetAllProductsAsync();
+    }
+}
---
+++ Infastucture/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using Application.Models.Product;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infastucture.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<List<ProductDto>> GetAllProductsAsync() =>
+            await _productRepository.GetAllProductsAsync();
+    }
+}
