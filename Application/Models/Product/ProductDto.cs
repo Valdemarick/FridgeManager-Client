d311bea567a1df8f0826e60b847bafee3d73368c
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 22:48:58 +0300

    created the product model

---
+++ Application/Models/Product/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Application.Models.Product
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
