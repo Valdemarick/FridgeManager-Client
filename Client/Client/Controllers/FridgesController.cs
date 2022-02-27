423cc570ca917c3b1dc844f9ebe4241785c84adf
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 14:30:58 +0300

    created the frdige controller

---
+++ Client/Client/Controllers/FridgesController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class FridgesController : Controller
+    {
+        private readonly IFridgeService _fridgeService;
+
+        public FridgesController(IFridgeService fridgeService)
+        {
+            _fridgeService = fridgeService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var fridges = await _fridgeService.GetAllFridgesAsync();
+
+            return View(fridges);
+        }
+    }
+}
