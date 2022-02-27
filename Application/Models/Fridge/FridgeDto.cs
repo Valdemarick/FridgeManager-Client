1e935a0ca71e856899026388ecc598fe867114df
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 22:29:42 +0300

    created folders for groups of models

---
+++ Application/Models/Fridge/FridgeDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application.Models.Fridge
+{
+    public class FridgeDto
+    {
+        public Guid Id { get; set; }
+        public string OwnerName { get; set; }
+        public string Manufacturer { get; set; }
+        public int ProductionYear { get; set; }
+    }
+}
---
+++ Application/Models/User/UserForAuthentication.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Models.User
+{
+    public class UserForAuthentication
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
