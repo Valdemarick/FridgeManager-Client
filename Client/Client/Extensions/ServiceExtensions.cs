b6880bb9ca211ae17a74ded6e813986a5edb17dc
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 12:22:21 +0300

    created extensions methods for injecting dependencies in DI container

---
+++ Client/Client/Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using Infastucture.Repositories;
+using Infastucture.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Client.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUserManagementRepository, UserManagementRepository>();
+        }
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserManagementService, UserManagementService>();
+        }
+    }
+}
