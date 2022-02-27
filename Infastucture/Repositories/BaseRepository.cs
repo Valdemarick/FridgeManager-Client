2796794098c5f611e7709fa8913e40c908002097
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 14:21:31 +0300

    created a base repository

---
+++ Infastucture/Repositories/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Flurl.Http;
+using Flurl.Http.Configuration;
+using Microsoft.AspNetCore.Http;
+
+namespace Infastucture.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly IFlurlClient _flurlClient;
+        protected readonly IHttpContextAccessor _httpContextAccessor;
+
+        public BaseRepository(IFlurlClientFactory flurlClientFactory, IHttpContextAccessor httpContextAccessor)
+        {
+            _flurlClient = flurlClientFactory.Get(Constants.ApiUrl);
+
+            _httpContextAccessor = httpContextAccessor;
+            var token = _httpContextAccessor.HttpContext.Request.Cookies[Constants.XAccesstoken];
+
+            if (!string.IsNullOrWhiteSpace(token))
+                _flurlClient.WithOAuthBearerToken(token);
+        }
+    }
+}
