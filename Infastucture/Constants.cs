85300d7deb20d39684859b50d3999b7b1e2a92ce
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 14:19:20 +0300

    created a class that contains constants

---
+++ Infastucture/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infastucture
+{
+    public static class Constants
+    {
+        public const string XAccesstoken = "X-Access-Token";
+        public const string ApiUrl = "https://localhost:5001/api";
+    }
+}
