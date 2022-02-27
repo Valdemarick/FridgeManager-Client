8cdf0d590e2974e137b8ba07966c8f3f12e4932e
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 14:25:17 +0300

    added new methods for injecting

--- Client/Client/Extensions/ServiceExtensions.cs
+++ Client/Client/Extensions/ServiceExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Interfaces.Repositories;
 using Application.Interfaces.Services;
+using Flurl.Http.Configuration;
 using Infastucture.Repositories;
 using Infastucture.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Client.Extensions
@@ -11,10 +13,19 @@ namespace Client.Extensions
         public static void ConfigureRepositories(this IServiceCollection services)
         {
             services.AddScoped<IUserManagementRepository, UserManagementRepository>();
+            services.AddScoped<IFridgeRepository, FridgeRepository>();
         }
+
         public static void ConfigureServices(this IServiceCollection services)
         {
             services.AddScoped<IUserManagementService, UserManagementService>();
+            services.AddScoped<IFridgeService, FridgeService>();
         }
+
+        public static void ConfigureHttpContextAccessor(this IServiceCollection services) =>
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        public static void ConfigureFlurlClient(this IServiceCollection services) =>
+            services.AddSingleton<IFlurlClientFactory, PerBaseUrlFlurlClientFactory>();
     }
 }
