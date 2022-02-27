f5b1b636b09d30451fee82bb929a261d2c76c75f
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-27 23:21:58 +0300

    created the product controller

---
+++ Client/Client/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProductsAsync();
+
+            return View(products);
+        }
+    }
+}
