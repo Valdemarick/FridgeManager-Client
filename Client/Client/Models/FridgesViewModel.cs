51b736ceea7582866473efbe495c1183ed011837
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-02 10:29:45 +0300

    created view models

---
+++ Client/Client/Models/FridgesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Application.Models.Fridge;
+using System.Collections.Generic;
+
+namespace Client.Models
+{
+    public class FridgesViewModel
+    {
+        public List<FridgeDto> Fridges { get; set; }
+    }
+}
---
+++ Client/Client/Models/ProductsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Application.Models.Product;
+using System.Collections.Generic;
+
+namespace Client.Models
+{
+    public class ProductsViewModel
+    {
+        public List<ProductDto> Products { get; set; }
+    }
+}
