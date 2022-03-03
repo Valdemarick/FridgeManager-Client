578d54d4f17ff8b49bcecc3454f0844d50e5da03
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-03 12:40:28 +0300

    created product model for update

---
+++ Client/Client/Models/Products/ProductForUpdateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.Models.Products
+{
+    public class ProductForUpdateViewModel
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
