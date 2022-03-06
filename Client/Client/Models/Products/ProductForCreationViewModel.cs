5101073809967d5203b2465f5a31c2512bdc9cae
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-06 13:25:46 +0300

    modified and added input data validation

--- Client/Client/Models/Products/ProductForCreationViewModel.cs
+++ Client/Client/Models/Products/ProductForCreationViewModel.cs
@@ -5,10 +5,11 @@ namespace Client.Models.Products
     public class ProductForCreationViewModel
     {
         [Required(ErrorMessage = "Name is required")]
+        [MaxLength(30, ErrorMessage = "Maximum length of Name is 30 characters")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Quantity is required")]
-        [Range(1, 9, ErrorMessage = "The value of quantity in the range between 1 and 9")]
+        [Range(1, 9, ErrorMessage = "The value of quantity must be in the range between 1 and 9")]
         public int Quantity { get; set; }
     }
 }
--- Client/Client/Models/Products/ProductForUpdateViewModel.cs
+++ Client/Client/Models/Products/ProductForUpdateViewModel.cs
@@ -1,14 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Client.Models.Products
 {
     public class ProductForUpdateViewModel
     {
+        [HiddenInput(DisplayValue = false)]
+        public Guid Id { get; set; }
+
         [Required(ErrorMessage = "Name is required")]
+        [MaxLength(30, ErrorMessage = "Maximum length of Name is 30 characters")]
         public string Name { get; set; }
-        
+
         [Required(ErrorMessage = "Quantity is required")]
-        [Range(1, 9, ErrorMessage = "The value of quantity in the range between 1 and 9")]
+        [Range(1, 9, ErrorMessage = "The value of quantity must be in the range between 1 and 9")]
         public int Quantity { get; set; }
     }
 }
