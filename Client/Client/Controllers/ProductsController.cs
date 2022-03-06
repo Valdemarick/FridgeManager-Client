39b21436ad0d7d87ec809633ec01b72c58e7917c
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-06 13:32:17 +0300

    comleted controller methods

--- Client/Client/Controllers/ProductsController.cs
+++ Client/Client/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Interfaces.Services;
-using Client.Models;
+using Client.Models.Products;
+using Domain.Entities.Products;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace Client.Controllers
@@ -22,6 +24,62 @@ namespace Client.Controllers
             };
 
             return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create", new ProductForCreationViewModel());
+        }
+
+        public IActionResult Update(ProductForUpdateViewModel productForUpdateViewModel)
+        {
+            return View("Update", productForUpdateViewModel);
+        }
+
+        [HttpPost]
+        [ActionName(nameof(CreateProductAsync))]
+        public async Task<IActionResult> CreateProductAsync(ProductForCreationViewModel productForCreationViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View("Create", productForCreationViewModel);
+
+            var productForCreation = new ProductForCreation()
+            {
+                Name = productForCreationViewModel.Name,
+                Quantity = productForCreationViewModel.Quantity,
+            };
+
+            await _productService.CreateProductAsync(productForCreation);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ActionName(nameof(DeleteProductAsync))]
+        public async Task<IActionResult> DeleteProductAsync(Guid id)
+        {
+            await _productService.DeleteProductAsync(id);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ActionName(nameof(UpdateProductAsync))]
+        public async Task<IActionResult> UpdateProductAsync(ProductForUpdateViewModel productForUpdateViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View("Update", productForUpdateViewModel);
+
+            var productForUpdate = new ProductForUpdate()
+            {
+                Id = productForUpdateViewModel.Id,
+                Name = productForUpdateViewModel.Name,
+                Quantity = productForUpdateViewModel.Quantity
+            };
+
+            await _productService.UpdateProductAsync(productForUpdate);
+
+            return RedirectToAction("Index");
         }
     }
 }
