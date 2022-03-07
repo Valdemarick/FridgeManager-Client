187e9a046ae32aa1922a3b2533ae2297633b7d0a
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-07 14:06:01 +0300

    created a base class and added inheritance from it

--- Client/Client/Models/Fridges/FridgeForCreationViewModel.cs
+++ Client/Client/Models/Fridges/FridgeForCreationViewModel.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace Client.Models.Fridges
+﻿namespace Client.Models.Fridges
 {
-    public class FridgeForCreationViewModel
+    public class FridgeForCreationViewModel : FridgeForManipulationViewModel
     {
-        [MaxLength(50, ErrorMessage = "Maximum length of 'OwnerName' is 50 characters")]
-        public string OwnerName { get; set; }
-
-        [MaxLength(200, ErrorMessage = "Maximum length of 'Description' is 200 characters")]
-        public string Description { get; set; }
-
-        [Required(ErrorMessage = "FridgeModel is required")]
-        public Guid FridgeModelId { get; set; }
     }
 }
---
+++ Client/Client/Models/Fridges/FridgeForUpdateViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Client.Models.Fridges
+{
+    public class FridgeForUpdateViewModel : FridgeForManipulationViewModel
+    {
+    }
+}
