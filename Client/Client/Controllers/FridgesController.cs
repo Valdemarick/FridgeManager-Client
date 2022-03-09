c38437978b2dcd94b594edf8edc8122339505f0b
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-09 10:16:58 +0300

    implemented DELETE and PUT methods

--- Client/Client/Controllers/FridgesController.cs
+++ Client/Client/Controllers/FridgesController.cs
@@ -1,6 +1,11 @@
-﻿using Application.Interfaces.Services;
-using Client.Models;
+﻿using Client.Models.Fridges;
+using Domain.Entities.FridgeModel;
+using Domain.Entities.Fridges;
+using Domain.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Client.Controllers
@@ -8,10 +13,12 @@ namespace Client.Controllers
     public class FridgesController : Controller
     {
         private readonly IFridgeService _fridgeService;
+        private readonly IFridgeModelService _fridgeModelService;
 
-        public FridgesController(IFridgeService fridgeService)
+        public FridgesController(IFridgeService fridgeService, IFridgeModelService fridgeModelService)
         {
             _fridgeService = fridgeService;
+            _fridgeModelService = fridgeModelService;
         }
 
         public async Task<IActionResult> Index()
@@ -23,5 +30,76 @@ namespace Client.Controllers
 
             return View(fridges);
         }
+
+        public async Task<IActionResult> Create()
+        {
+            var models = await GetModels();
+            SelectList modelsList = new SelectList(models, "Id", "Name");
+
+            ViewBag.Models = modelsList;
+
+            return View("Create", new FridgeForCreationViewModel());
+        }
+
+        public async Task<IActionResult> Update(FridgeForUpdateViewModel fridgeForUpdateViewModel)
+        {
+            var models = await GetModels();
+            SelectList modelsList = new SelectList(models, "Id", "Name");
+
+            ViewBag.Models = modelsList;
+
+            return View("Update", fridgeForUpdateViewModel);
+        }
+
+        [HttpPost]
+        [ActionName(nameof(CreateFridgeAsync))]
+        public async Task<IActionResult> CreateFridgeAsync(FridgeForCreationViewModel fridgeForCreationViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View("Create", fridgeForCreationViewModel);
+
+            var fridgeForCreation = new FridgeForCreation()
+            {
+                FridgeModelId = fridgeForCreationViewModel.FridgeModelId,
+                OwnerName = fridgeForCreationViewModel?.OwnerName,
+                Description = fridgeForCreationViewModel?.Description,
+            };
+
+            await _fridgeService.CreateFridgeAsync(fridgeForCreation);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ActionName(nameof(UpdateFridgeAsync))]
+        public async Task<IActionResult> UpdateFridgeAsync(FridgeForUpdateViewModel fridgeForUpdateViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View("Update", fridgeForUpdateViewModel);
+
+            var fridgeForUpdate = new FridgeForUpdate()
+            {
+                Id = fridgeForUpdateViewModel.Id,
+                FridgeModelId = fridgeForUpdateViewModel.FridgeModelId,
+                OwnerName = fridgeForUpdateViewModel?.OwnerName,
+                Description = fridgeForUpdateViewModel?.Description
+            };
+
+            await _fridgeService.UpdateFridgeAsync(fridgeForUpdate);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ActionName(nameof(DeleteFridgeAsync))]
+        public async Task<IActionResult> DeleteFridgeAsync(Guid id)
+        {
+            await _fridgeService.DeleteFridgeAsync(id);
+
+            return RedirectToAction("Index");
+        }
+
+        private async Task<List<FridgeModel>> GetModels() =>
+            await _fridgeModelService.GetAllFridgeModelsAsync();
     }
 }
