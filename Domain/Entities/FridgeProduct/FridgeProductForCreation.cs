f069a6766f5211175115ab6bd92ebd8e087c591e
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-09 10:30:24 +0300

    created 'FridgeProduct' model

---
+++ Domain/Entities/FridgeProduct/FridgeProductForCreation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain.Entities.FridgeProduct
+{
+    public class FridgeProduct
+    {
+        public Guid FridgeId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
