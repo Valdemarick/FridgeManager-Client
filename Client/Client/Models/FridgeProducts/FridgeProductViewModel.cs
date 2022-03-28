4ac56d06e4619244f6b75a794e96352a5551606d
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-28 16:46:15 +0300

    created fridge products models

---
+++ Client/Client/Models/FridgeProducts/FridgeProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities.FridgeProduct;
+using System.Collections.Generic;
+
+namespace Client.Models.FridgeProducts
+{
+    public class FridgeProductViewModel
+    {
+        public List<FridgeProduct> FridgeProducts { get; set; }
+    }
+}
---
+++ Domain/Entities/FridgeProduct/FridgeProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Entities.FridgeProduct
+{
+    public class FridgeProduct
+    {
+        public Guid Id { get; set; }
+        public Guid FridgeId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
