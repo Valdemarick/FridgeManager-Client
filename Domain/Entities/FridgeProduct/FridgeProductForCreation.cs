3ab725f1651371deb7136cbc2f2776e891239872
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-28 16:44:39 +0300

    added new prop. int replaced with nullable<int>

--- Domain/Entities/FridgeProduct/FridgeProductForCreation.cs
+++ Domain/Entities/FridgeProduct/FridgeProductForCreation.cs
@@ -2,10 +2,11 @@
 
 namespace Domain.Entities.FridgeProduct
 {
-    public class FridgeProduct
+    public class FridgeProductForCreation
     {
         public Guid FridgeId { get; set; }
         public Guid ProductId { get; set; }
-        public int Quantity { get; set; }
+        public string ProductName { get; set; }
+        public int? Quantity { get; set; }
     }
 }
