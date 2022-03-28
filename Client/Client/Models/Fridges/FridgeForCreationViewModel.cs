e7f6da9e993c9e9a19d18c3f36daa7c8c2cedeae
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-28 16:42:56 +0300

    added new props

--- Client/Client/Models/Fridges/FridgeForCreationViewModel.cs
+++ Client/Client/Models/Fridges/FridgeForCreationViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace Client.Models.Fridges
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace Client.Models.Fridges
 {
     public class FridgeForCreationViewModel : FridgeForManipulationViewModel
     {
+        public MultiSelectList Products { get; set; }
+        public IEnumerable<Guid> ProductIds { get; set; }
     }
 }
