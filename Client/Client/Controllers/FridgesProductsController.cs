44fbfb7e24d69077fb03863cafd94426b2aedce2
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-30 12:10:45 +0300

    created fridge product controller. created GET method

---
+++ Client/Client/Controllers/FridgesProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Client.Models.FridgeProducts;
+using Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class FridgesProductsController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public FridgesProductsController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        public ViewResult GetByFridgeId(FridgeProductViewModel fridgeProductsViewModel)
+        {
+            return View(fridgeProductsViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductsByFridgeIdAsync(Guid fridgeId)
+        {
+            var fridgeProductsViewModel = new FridgeProductViewModel()
+            {
+                FridgeProducts = await _serviceManager.FridgeProductService.GetFridgeProductsByFridgeId(fridgeId)
+            };
+
+            return RedirectToAction(nameof(GetByFridgeId), new { fridgeProductsViewModel = fridgeProductsViewModel });
+        }
+    }
+}
