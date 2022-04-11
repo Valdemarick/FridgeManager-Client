88228dd697b14712be63849f1f8fd933ed5500e1
Valdemarick <korolev.vadim.2017@mail.ru>
2022-04-11 11:33:27 +0300

    created product model for DELETE action

---
+++ Client/Client/Models/Products/ProductForDeleteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models.Products
+{
+    public class ProductForDeleteViewModel
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public Guid Id { get; set; }
+    }
+}
