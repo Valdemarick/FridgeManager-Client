014979e28365eb778f3f583cb8bf4f31f8fdd116
Valdemarick <korolev.vadim.2017@mail.ru>
2022-04-11 11:37:12 +0300

    created fridge model for DELETE action

---
+++ Client/Client/Models/Fridges/FridgeForDeleteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models.Fridges
+{
+    public class FridgeForDeleteViewModel
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public Guid Id { get; set; }
+    }
+}
