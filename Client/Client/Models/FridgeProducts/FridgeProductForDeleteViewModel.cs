4bf1968a964dc13803b9daa1f2ddc306d8cb2675
Valdemarick <korolev.vadim.2017@mail.ru>
2022-04-13 21:45:29 +0300

    created view model for DELETE

---
+++ Client/Client/Models/FridgeProducts/FridgeProductForDeleteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models.FridgeProducts
+{
+    public class FridgeProductForDeleteViewModel
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public Guid Id { get; set; }
+    }
+}
